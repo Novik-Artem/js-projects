f8f5a4c694bceb2a66295c50f117154ac09d33e6
Novik-Artem <artemgithab@gmail.com>
2022-02-01 17:01:19 +0300

    JS-3: - pomodoro

--- webpack.config.js
+++ webpack.config.js
@@ -4,32 +4,36 @@ const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const isDev = process.env.NODE_ENV === "development";
 const isProd = !isDev;
-const filename = (ext) => isDev ? `[name].${ext}` : `[name].[contenthash].${ext}`;
+const filename = (ext) =>
+  isDev ? `[name].${ext}` : `[name].[contenthash].${ext}`;
 module.exports = {
   context: path.resolve(__dirname, "src"),
-  entry: ["@babel/polyfill","./js/index.js"],
+  entry: ["@babel/polyfill", "./js/index.js"],
   module: {
     rules: [
       { test: /\.css$/, use: [MiniCssExtractPlugin.loader, "css-loader"] },
       { test: /\.(svg|ico|png|webp|jpg|gif|jpeg)$/, type: "asset/resource" },
-			{ test: /\.html$/, use: ["html-loader"] },
-			{ test: /\.s[ac]ss$/, use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"] },
-			{
+      { test: /\.html$/, use: ["html-loader"] },
+      {
+        test: /\.s[ac]ss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+      {
         test: /\.m?js$/,
         exclude: /node_modules/,
         use: {
           loader: "babel-loader",
           options: {
-						presets: ['@babel/preset-env'],
-						plugins: ["@babel/plugin-proposal-class-properties"]
-          }
-        }
+            presets: ["@babel/preset-env"],
+            plugins: ["@babel/plugin-proposal-class-properties"],
+          },
+        },
       },
     ],
   },
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: `./js/${filename('js')}`,
+    filename: `./js/${filename("js")}`,
   },
   devServer: {
     port: 8080,
@@ -37,14 +41,14 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-			template: path.resolve(__dirname, "src/index.html"),
-			minify: {
-				collapseWhitespace: isProd,
-			}
+      template: path.resolve(__dirname, "src/index.html"),
+      minify: {
+        collapseWhitespace: isProd,
+      },
     }),
     new CleanWebpackPlugin(),
     new MiniCssExtractPlugin({
-      filename: `./css/${filename('css')}`,
+      filename: `./css/${filename("css")}`,
     }),
   ],
   mode: process.env.NODE_ENV === "production" ? "production" : "development",
